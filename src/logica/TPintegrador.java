9fd5c84b9f96c66feb7f4dd5b580a77559c17dcc
Sam-Blz <jossy_b2004@outlook.com>
2026-08-16 19:43:23 -0500

    sortear

---
+++ src/logica/TPintegrador.java
@@ -0,0 +1,16 @@
+
+package logica;
+
+import igu.Principal;
+
+
+public class TPintegrador {
+
+    
+    public static void main(String[] args) {
+       Principal ventana=new Principal();
+       ventana.setVisible(true);
+       ventana.setLocationRelativeTo(null);
+    }
+    
+}
